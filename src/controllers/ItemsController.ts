472c9fafc1d8d299907b0097254c38ba8b40a8ad
armendes <armendes@indracompany.com>
2020-06-05 13:41:58 -0300

    Editing url for image item

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -8,8 +8,9 @@ class ItemsController {
 
         const serializedItems = items.map( item => {
             return {
+                id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`
+                image_url: `http://10.0.0.86:3333/uploads/${item.image}`
             };
         });
 
